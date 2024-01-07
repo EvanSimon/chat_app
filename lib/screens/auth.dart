dd8df41d846e2d4284d9bca2523c25031a5ea1af
EvanSimon <41878176+EvanSimon@users.noreply.github.com>
2024-01-07 17:49:12 -0500

    Adding user image and spiner

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -17,22 +21,37 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
   var _isLogin = true;
+  File? _selectedImage;
+  var _isAuthenticating = false;
 
   void _submit() async {
     final isVaild = _form.currentState!.validate();
 
-    if (!isVaild) {
+    if (!isVaild || !_isLogin && _selectedImage == null) {
       return;
     }
+
     _form.currentState!.save();
 
     try {
+      setState(() {
+        _isAuthenticating = true;
+      });
       if (_isLogin) {
         final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
       } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+        print(imageUrl);
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -44,6 +63,9 @@ class _AuthScreenState extends State<AuthScreen> {
           content: Text(error.message ?? 'Authentication failed.'),
         ),
       );
+      setState(() {
+        _isAuthenticating = false;
+      });
     }
   }
 
@@ -76,6 +98,12 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if (!_isLogin)
+                            UserImagePicker(
+                              onPickImage: (pickedImage) {
+                                _selectedImage = pickedImage;
+                              },
+                            ),
                           TextFormField(
                             decoration: const InputDecoration(
                                 labelText: 'Email Adress'),
@@ -111,6 +139,9 @@ class _AuthScreenState extends State<AuthScreen> {
                           const SizedBox(
                             height: 12,
                           ),
+                          if(_isAuthenticating)
+                            const CircularProgressIndicator();
+                          if(!_isAuthenticating)
                           ElevatedButton(
                             onPressed: _submit,
                             style: ElevatedButton.styleFrom(
@@ -119,6 +150,7 @@ class _AuthScreenState extends State<AuthScreen> {
                                     .primaryContainer),
                             child: Text(_isLogin ? "Login" : 'Sign up'),
                           ),
+                          if(!_isAuthenticating)
                           TextButton(
                             onPressed: () {
                               setState(() {
