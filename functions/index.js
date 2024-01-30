d485d6ad9524360157f264509063e049f898ffb9
EvanSimon <41878176+EvanSimon@users.noreply.github.com>
2024-01-30 18:33:30 -0500

    cloud function to resend push notifaction

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,41 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// Cloud Firestore triggers ref: https://firebase.google.com/docs/functions/firestore-events
+const sendPushNotification = (snapshot) => {
+  return admin.messaging().sendToTopic("chat", {
+    // Sending a notification message.
+    notification: {
+      title: snapshot.data()["username"],
+      body: snapshot.data()["text"],
+      clickAction: "FLUTTER_NOTIFICATION_CLICK",
+    },
+  });
+};
 exports.myFunction = functions.firestore
     .document("chat/{messageId}")
-    .onCreate((snapshot, context) => {
-    // Return this function's promise, so this ensures the firebase function
-    // will keep running, until the notification is scheduled.
+    .onCreate(async (snapshot, context) => {
+      const userId = context.params.userId;
+      const notificationData = context.after.val();
+      // Check if the user has pressed the notification
+      if (notificationData && notificationData.pressed) {
+        console.log("User has pressed the notification. No need to resend.");
+        return null;
+      }
+      // Wait for 3 minutes (180,000 milliseconds) before resending
+      await new Promise((resolve) => setTimeout(resolve, 180000));
+      // (replace 'userTokenPath' with your data structure)
+      const userTokenPath = `/chat/${userId}/fcmToken`;
+      const userSnapshot =
+      await admin.database().ref(userTokenPath).once("value");
+      const userToken = userSnapshot.val();
+      if (userToken) {
+        // Resend the push notification
+        await sendPushNotification(userToken,
+            "Reminder: Your notification message.");
+        console.log("Push notification resent.");
+      } else {
+        console.error("User has no FCM token.");
+      }
       return admin.messaging().sendToTopic("chat", {
       // Sending a notification message.
         notification: {
